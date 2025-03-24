e1fd815905296d0e157c9d7b3e34d562d9d80906
darshilkantaria11 <darshilkantaria11@gmail.com>
2025-03-24 18:39:15 +0530

    optimized

--- app/nopage/food/food2.jsx
+++ app/nopage/food/food2.jsx
@@ -7,7 +7,7 @@ import C from '../../../public/f3.svg';
 export default function DayAtPreschool() {
     return (
         <>
-        <div className="flex mt-20 mb-10 justify-center items-center text-3xl md:text-6xl lv">
+        <div className="flex mt-8 mb-10 justify-center items-center text-3xl md:text-6xl lv">
             <h1 className='text-center text-g4'>Snack and Meal Schedule</h1>
         </div>
             <div className="container mx-auto grid grid-cols-2 gap-4 md:grid-cols-3 lg:gap-8 ">
