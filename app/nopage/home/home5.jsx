24b0d4420d5493fb2c63b24558337d6cdd26eafe
darshilkantaria11 <darshilkantaria11@gmail.com>
2025-03-24 19:46:56 +0530

    optimized

--- app/nopage/home/home5.jsx
+++ app/nopage/home/home5.jsx
@@ -1,56 +1,28 @@
-import Image from 'next/image';
-import A from '../../../public/h5a.svg'; // Import the SVG frame
-import B from '../../../public/h5b.svg';
-import C from '../../../public/h5c.svg';
-import D from '../../../public/h5d.svg';
-import E from '../../../public/h5e.svg';
-import F from '../../../public/h5f.svg';
-
 export default function DayAtPreschool() {
     return (
         <>
-        <div className="flex mt-20 mb-10 justify-center items-center text-3xl md:text-5xl lv">
-            <h1 className='text-center text-g4'>A Day at Our Preschool</h1>
-        </div>
+            <div className="flex mt-20 mb-10 justify-center items-center text-3xl md:text-5xl lv">
+                <h1 className="text-center text-g4">A Day at Our Preschool</h1>
+            </div>
             <div className="container mx-auto grid grid-cols-2 gap-4 lg:grid-cols-3 lg:gap-8">
-                <div className="hover:scale-105 transition-transform duration-200 ease-in-out ">
-                    <img
-                    src={A}
-                    alt='play time'
-                    />
+                <div className="hover:scale-105 transition-transform duration-200 ease-in-out">
+                    <img src="/h5a.svg" alt="play time" className="w-full h-auto" />
                 </div>
-                <div className="hover:scale-105 transition-transform duration-200 ease-in-out ">
-                    <Image
-                    src={B}
-                    alt='outdoor time'
-                    unoptimized></Image>
+                <div className="hover:scale-105 transition-transform duration-200 ease-in-out">
+                    <img src="/h5b.svg" alt="outdoor time" className="w-full h-auto" />
                 </div>
-                <div className="hover:scale-105 transition-transform duration-200 ease-in-out ">
-                    <Image
-                    src={C}
-                    alt='circle time'
-                    unoptimized ></Image>
+                <div className="hover:scale-105 transition-transform duration-200 ease-in-out">
+                    <img src="/h5c.svg" alt="circle time" className="w-full h-auto" />
                 </div>
-                <div className="hover:scale-105 transition-transform duration-200 ease-in-out ">
-                    <Image
-                    src={D}
-                    alt='quite time'
-                    unoptimized ></Image>
+                <div className="hover:scale-105 transition-transform duration-200 ease-in-out">
+                    <img src="/h5d.svg" alt="quiet time" className="w-full h-auto" />
                 </div>
-                <div className="hover:scale-105 transition-transform duration-200 ease-in-out ">
-                    <Image
-                    src={E}
-                    alt='snack time'
-                    unoptimized ></Image>
+                <div className="hover:scale-105 transition-transform duration-200 ease-in-out">
+                    <img src="/h5e.svg" alt="snack time" className="w-full h-auto" />
                 </div>
-                <div className=" hover:scale-105 transition-transform duration-200 ease-in-out">
-                    <Image
-                    src={F}
-                    alt='lunch time'
-                    unoptimized ></Image>
+                <div className="hover:scale-105 transition-transform duration-200 ease-in-out">
+                    <img src="/h5f.svg" alt="lunch time" className="w-full h-auto" />
                 </div>
-                
-               
             </div>
         </>
     );
