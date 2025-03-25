0fcfb2ed4b0e6c7057b2e6df94c7cc3586f20959
darshilkantaria11 <darshilkantaria11@gmail.com>
2025-03-25 22:14:46 +0530

    optimized

--- app/nopage/home/home6.jsx
+++ app/nopage/home/home6.jsx
@@ -6,7 +6,7 @@ export default function ProgramCard() {
         <div className="flex p-2 container mx-auto justify-center items-center py-10 lv">
             <div className=" flex flex-col bg-g1 text-white w-full rounded-3xl px-8 py-10 shadow-lg">
                 <h2 className="text-3xl md:text-4xl font-bold text-center">
-                    Programs for Toddlers (Ages 19 Months to 2 Years)
+                    Programs Offered (Ages 19 Months to 12 Years)
                 </h2>
 
                 <div className="mt-6 text-center text-lg xl:text-2xl">
@@ -19,7 +19,7 @@ export default function ProgramCard() {
 
                 <div className="mt-8 flex justify-center">
                     <Link
-                        href="/program/toddler"
+                        href="/program"
                         className="inline-block p-1 bg-y1 text-yellow-900 font-semibold rounded-full hover:bg-yellow-500 transition"
                     >
                         <p className='border-dashed border-2 border-yellow-700 xl:px-4 xl:py-2 px-2 py-1 rounded-full text-sm xl:text-3xl'>Get to Know More</p>
